bb1be7cd6370f6a966b0d926ece52a6178dd2d9f
John Guo <39734716+johnguolex@users.noreply.github.com>
2023-09-06 14:58:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'Not yet! Last updated 09/06/23';
+  return <div>{value}</div>;
 }
 
 export default App;
